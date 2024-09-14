31346b2832319c808df49d17b95b8f8d61a20f89
amanabiev10 <amanabiev10@gmail.com>
2024-09-15 01:53:09 +0200

    Dockerfile Backend9

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import ListPosts
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import ListPosts, UpdatePost, DeletePost
+
 
 urlpatterns = [
     path('posts/', ListPosts.as_view(), name='list-posts'),
-    path('posts/<int:pk>/', ListPosts.as_view(), name='update-post'),
-    path('posts/<int:pk>/delete/', ListPosts.as_view(), name='delete-post'),
+    path('posts/<int:pk>/', UpdatePost.as_view(), name='update-post'),
+    path('posts/<int:pk>/delete/', DeletePost.as_view(), name='delete-post'),
 ]
